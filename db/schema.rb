205ea777eb595a2b05122042c968c1ce9bf153de
Malo Bobrowska <malo.bobrow@gmail.com>
2023-03-01 11:39:58 +0100

    seed ans schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_28_144511) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,11 +66,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_144511) do
     t.string "character"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "review"
     t.string "address"
     t.string "photo_url"
     t.string "name"
     t.string "species"
+    t.text "review"
     t.index ["user_id"], name: "index_dinosaurs_on_user_id"
   end
 
