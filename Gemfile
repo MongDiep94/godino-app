3273671d1333fd6070da19475781a611e3fd1d3a
Nicolas BRIEN <brien.nicolas@gmail.com>
2023-03-01 12:15:56 +0100

    set up

--- Gemfile
+++ Gemfile
@@ -55,6 +55,8 @@ gem "dotenv-rails", groups: [:development, :test]
 
 gem "cloudinary"
 
+gem "geocoder"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_144511) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_105109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_144511) do
     t.string "photo_url"
     t.string "name"
     t.string "species"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_dinosaurs_on_user_id"
   end
 
