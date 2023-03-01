063561dfe28f6c74061a8ca6b47fa0b88e945eff
Nicolas BRIEN <brien.nicolas@gmail.com>
2023-03-01 15:12:22 +0100

    mapbox added to index and show with style

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -3,6 +3,8 @@ class Dinosaur < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def average_rating
     return 1
