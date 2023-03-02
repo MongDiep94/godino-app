8efe2c402ef345cd18a9429ea46714e8f7326523
Nicolas BRIEN <brien.nicolas@gmail.com>
2023-03-02 12:38:22 +0100

    searchbar created

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -2,18 +2,24 @@ class Dinosaur < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
-
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
   pg_search_scope :search_by_name_species_and_character,
-  against: [ :name, :species, :character ],
-  using: {
-    tsearch: { prefix: true }
-  }
+    against: {
+      name: 'A',
+      species: 'B',
+      character: 'C'
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   def average_rating
     return 4
     # self.bookings.pluck(:rating) / self.bookings.length
